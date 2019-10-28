34dedd364c721992f859a144be62dcf876f09089
ben_jacobson@live.com <ben_jacobson@live.com>
2019-10-28 16:17:01 +1100

    created user interface for listView with drag and drop and sort feature

---
+++ bookmarks/static/vendor_js/jquery.enable_sortable.js
@@ -0,0 +1,18 @@
+// set an event - when you hover over the move icon, enables dragging and dropping
+$('.move-icon').mouseenter(function() {
+    $('.thumbnail-sortable').sortable('enable');
+});
+
+// set an event - when you leave the move icon, disables dragging and dropping
+$('.move-icon').mouseleave(function() {
+    $('.thumbnail-sortable').sortable('disable');
+});
+
+// set up our sortable and start up in disabled mode
+$(function() {
+    $('.thumbnail-sortable').sortable({
+        placeholderClass: 'col-sm-6 col-md-6' // tells us how big the placeholder blank needs to be
+    });
+
+    $('.thumbnail-sortable').sortable('disable'); // start in disabled state   
+});
